59911f9506bcb0b4ffc4123274c3ebe30e5d8e61
FelipeleonB <fleon@correo.iue.edu.co>
2024-08-12 02:11:03 -0500

    Add files via upload
    bn

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Turnos.Forms;
+
+namespace Turnos
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //para poner el desarrollo en el form deseado
+            //Application.Run(new AreaSolicitada());
+            Application.Run(new frmInicio());
+           // Application.Run(new frmRegistro());
+
+        }
+    }
+}
